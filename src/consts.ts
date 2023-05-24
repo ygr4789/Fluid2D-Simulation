fcd4dda37d79a7724be0b978f2a29771dcc3cfb9
ygr4789 <yongdori@snu.ac.kr>
2023-05-25 01:07:51 +0900

    Applied gravity

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,28 @@
+import * as glm from "gl-matrix";
+
+export const WIDTH = 600;
+export const HEIGHT = 400;
+
 export const WATER_DENSITY = 998;
 export const WATER_COLOR = "#0000ff";
+
+export const KERNEL_DISTANCE = 10;
+export const TIMESTEP = 0.013;
+export const GRAVITY = glm.vec2.fromValues(0, -980);
+export const RESTITUTION = 0.5;
+
+export function poly6Kernel(r: glm.vec2, h: number) {
+  let d = glm.vec2.len(r);
+  return (315 / 64 / Math.PI / h ** 9) * (h ** 2 - d ** 2) ** 3;
+}
+
+export function poly6Grad(r: glm.vec2, h: number) {
+  let d = glm.vec2.len(r);
+  let ret = glm.vec2.clone(r);
+  glm.vec2.scale(ret, ret, (-(45 / Math.PI / h ** 6) * (h - d) ** 2) / d ** 2);
+}
+
+export function poly6Lap(r: glm.vec2, h: number) {
+  let d = glm.vec2.len(r);
+  return (45 / Math.PI / h ** 6) * (h - d);
+}
--- src/main.ts
+++ src/main.ts
@@ -3,33 +3,31 @@ import * as glm from "gl-matrix";
 /** @type {HTMLCanvasElement} */
 
 import "../style/style.css";
-import { WATER_COLOR, WATER_DENSITY } from "./consts";
+import { GRAVITY, HEIGHT, RESTITUTION, TIMESTEP, WATER_COLOR, WATER_DENSITY, WIDTH } from "./consts";
 import { Particle } from "./particle";
 
-let width = 600;
-let height = 400;
 let particles: Array<Particle> = [];
 
 function sizeCanvas() {
   const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
   // width = window.innerWidth;
   // height = window.innerHeight;
-  canvas.width = width;
-  canvas.height = height;
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
 }
 
 function drawParticle(ctx: CanvasRenderingContext2D, p: Particle) {
   ctx.fillStyle = WATER_COLOR;
   ctx.beginPath();
-  ctx.arc(p.pos[0], p.pos[1], 5, 0, 2 * Math.PI);
+  ctx.arc(p.pos[0], HEIGHT - p.pos[1], 5, 0, 2 * Math.PI);
   // ctx.stroke();
   ctx.fill();
 }
 
-function initParticles(d: number) {
-  for (let x = d / 2; x < 100; x += d) {
-    for (let y = d / 2; y < 300; y += d) {
-      particles.push(new Particle(x, height - y, WATER_DENSITY * d ** 2));
+function initParticles(size: number) {
+  for (let x = size / 2; x < 100; x += size) {
+    for (let y = size / 2; y < 300; y += size) {
+      particles.push(new Particle(x, y, WATER_DENSITY * size ** 2));
     }
   }
 }
@@ -38,23 +36,64 @@ function render() {
   const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
   const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
   // Clear the canvas and redraw all particles
-  ctx.clearRect(0, 0, width, height);
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
   particles.forEach((p) => {
     drawParticle(ctx, p);
   });
 }
 
 function getNearNeighbors(loc: glm.vec2, dist: number) {
-  let ret:Array<Particle> = [];
+  let ret: Array<Particle> = [];
   particles.forEach((p_) => {
-    if(glm.vec2.dist(loc, p_.pos) < dist) particles.push(p_)
-  })
-  return 
+    if (glm.vec2.dist(loc, p_.pos) < dist) particles.push(p_);
+  });
+  return ret;
+}
+
+function computeDensity() {}
+function computePressure() {}
+function computeAcceleration() {
+  particles.forEach((p) => {
+    glm.vec2.copy(p.acc, GRAVITY);
+  });
+}
+function updateParcitles(dt: number) {
+  particles.forEach((p) => {
+    p.update(dt);
+  });
+}
+function handleBoundaries() {
+  particles.forEach((p) => {
+    if (p.pos[0] < 0) {
+      p.pos[0] = 0;
+      p.vel[0] *= -RESTITUTION;
+    }
+    if (p.pos[0] > WIDTH) {
+      p.pos[0] = WIDTH;
+      p.vel[0] *= -RESTITUTION;
+    }
+    if (p.pos[1] < 0) {
+      p.pos[1] = 0;
+      p.vel[1] *= -RESTITUTION;
+    }
+    if (p.pos[1] > HEIGHT) {
+      p.pos[1] = HEIGHT;
+      p.vel[1] *= -RESTITUTION;
+    }
+  });
+}
+
+function simulate() {
+  computeDensity();
+  computePressure();
+  computeAcceleration();
+  updateParcitles(TIMESTEP);
+  handleBoundaries();
 }
 
 // Main animation loop
 function animationLoop() {
-  // updateParcitles()
+  simulate();
   render();
   // Schedule the next frame
   window.requestAnimationFrame(animationLoop);
