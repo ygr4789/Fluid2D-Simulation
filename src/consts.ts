4d10b35c492be07f2183c12151d4b4de38da217f
ygr4789 <yongdori@snu.ac.kr>
2023-05-25 18:40:08 +0900

    Spatial hashing

--- src/consts.ts
+++ src/consts.ts
@@ -3,34 +3,36 @@ import * as glm from "gl-matrix";
 export const WIDTH = 600;
 export const HEIGHT = 400;
 
-export const WATER_DENSITY = 0.15;
-export const WATER_PARTICLE_MASS = 200;
-export const WATER_K_FACTOR = 20000;
+export const WATER_DENSITY = 998;
+export const WATER_K_FACTOR = 50000;
 export const WATER_COLOR = "#0000ff";
-export const WATER_VISCOSITY = 50;
+export const WATER_VISCOSITY = 200;
 
-export const KERNEL_DISTANCE = 8;
+export const KERNEL_DISTANCE = 10;
 export const TIMESTEP = 0.013;
-export const GRAVITY = glm.vec2.fromValues(0, -10);
+export const GRAVITY = glm.vec2.fromValues(0, -980);
 export const RESTITUTION = 0.5;
 
-export function poly6Kernel(r: glm.vec2, h: number) {
+const KERNEL_FACTOR = 315 / 64 / Math.PI / KERNEL_DISTANCE ** 9;
+export function poly6Kernel(r: glm.vec2) {
   let d = glm.vec2.len(r);
-  return (315 / 64 / Math.PI / h ** 9) * (h ** 2 - d ** 2) ** 3;
+  return KERNEL_FACTOR * (KERNEL_DISTANCE ** 2 - d ** 2) ** 3;
 }
 
-export function poly6Grad(r: glm.vec2, h: number) {
+const GRAD_FACTOR = -45 / Math.PI / KERNEL_DISTANCE ** 6;
+export function poly6Grad(r: glm.vec2) {
   let d = glm.vec2.len(r);
   let ret = glm.vec2.clone(r);
   if (d === 0) {
     glm.vec2.zero(ret);
     return ret;
   }
-  glm.vec2.scale(ret, ret, (-(45 / Math.PI / h ** 6) * (h - d) ** 2) / d);
+  glm.vec2.scale(ret, ret, (GRAD_FACTOR * (KERNEL_DISTANCE - d) ** 2) / d);
   return ret;
 }
 
-export function poly6Lap(r: glm.vec2, h: number) {
+const LAP_FACTOR = 45 / Math.PI / KERNEL_DISTANCE ** 6
+export function poly6Lap(r: glm.vec2) {
   let d = glm.vec2.len(r);
-  return (45 / Math.PI / h ** 6) * (h - d);
+  return LAP_FACTOR * (KERNEL_DISTANCE - d);
 }
--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,6 @@ import {
   WATER_COLOR,
   WATER_DENSITY,
   WATER_K_FACTOR,
-  WATER_PARTICLE_MASS,
   WATER_VISCOSITY,
   poly6Grad,
   poly6Kernel,
@@ -22,6 +21,49 @@ import {
 import { Particle } from "./particle";
 
 let particles: Array<Particle> = [];
+let hashTable: Array<Array<Particle>>;
+
+let max_hi = HEIGHT / KERNEL_DISTANCE + 1;
+let max_wi = WIDTH / KERNEL_DISTANCE + 1;
+
+function updateHashTable() {
+  hashTable = new Array(max_hi * max_wi);
+  for (let i = 0; i < hashTable.length; i++) {
+    hashTable[i] = new Array();
+  }
+  particles.forEach((p) => {
+    let hi = Math.floor(p.pos[1] / KERNEL_DISTANCE);
+    let wi = Math.floor(p.pos[0] / KERNEL_DISTANCE);
+    let cell = hashTable[hi * max_hi + wi];
+    cell.push(p);
+  });
+}
+
+const dh = [-1, -1, -1, 0, 0, 0, 1, 1, 1];
+const dw = [-1, 0, 1, -1, 0, 1, -1, 0, 1];
+function hashNearNeighbors(loc: glm.vec2) {
+  let ret: Array<Particle> = [];
+  let hi = Math.floor(loc[1] / KERNEL_DISTANCE);
+  let wi = Math.floor(loc[0] / KERNEL_DISTANCE);
+  for (let i = 0; i < 9; i++) {
+    let hi_ = hi + dh[i];
+    let wi_ = wi + dw[i];
+    if (hi_ < 0 || hi_ >= max_hi) continue;
+    if (wi_ < 0 || wi_ >= max_wi) continue;
+    hashTable[hi_ * max_hi + wi_].forEach((p) => {
+      if (glm.vec2.dist(loc, p.pos) < KERNEL_DISTANCE) ret.push(p);
+    });
+  }
+  return ret;
+}
+
+function getNearNeighbors(loc: glm.vec2) {
+  let ret: Array<Particle> = [];
+  particles.forEach((p) => {
+    if (glm.vec2.dist(loc, p.pos) < KERNEL_DISTANCE) ret.push(p);
+  });
+  return ret;
+}
 
 function sizeCanvas() {
   const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
@@ -34,19 +76,31 @@ function sizeCanvas() {
 function drawParticle(ctx: CanvasRenderingContext2D, p: Particle) {
   ctx.fillStyle = WATER_COLOR;
   ctx.beginPath();
-  ctx.arc(p.pos[0], HEIGHT - p.pos[1], 2, 0, 2 * Math.PI);
+  ctx.arc(p.pos[0], HEIGHT - p.pos[1], 1, 0, 2 * Math.PI);
   // ctx.stroke();
   ctx.fill();
 }
 
 function initParticles(size: number) {
-  for (let x = size / 2; x < 100; x += size) {
-    for (let y = size / 2; y < 300; y += size) {
-      particles.push(
-        new Particle(x + (Math.random() * size) / 10, y + (Math.random() * size) / 10, WATER_PARTICLE_MASS)
-      );
+  const left = 0;
+  const right = 100;
+  const bottom = 0;
+  const top = 300;
+  for (let x = left + size / 2; x < right; x += size) {
+    for (let y = bottom + size / 2; y < top; y += size) {
+      particles.push(new Particle(x + (Math.random() * size) / 10, y + (Math.random() * size) / 10, 1));
     }
   }
+  updateHashTable();
+  computeDensity();
+  let max_density = 0;
+  particles.forEach((p) => {
+    max_density = Math.max(max_density, p.density);
+  });
+  const WATER_PARTICLE_MASS = WATER_DENSITY / max_density;
+  particles.forEach((p) => {
+    p.mass = WATER_PARTICLE_MASS;
+  });
 }
 
 function render() {
@@ -59,21 +113,13 @@ function render() {
   });
 }
 
-function getNearNeighbors(loc: glm.vec2, dist: number) {
-  let ret: Array<Particle> = [];
-  particles.forEach((p_) => {
-    if (glm.vec2.dist(loc, p_.pos) < dist) ret.push(p_);
-  });
-  return ret;
-}
-
 function computeDensity() {
   let r = glm.vec2.create();
   particles.forEach((p) => {
     p.density = 0;
-    getNearNeighbors(p.pos, KERNEL_DISTANCE).forEach((p_) => {
+    hashNearNeighbors(p.pos).forEach((p_) => {
       glm.vec2.sub(r, p.pos, p_.pos);
-      p.density += p_.mass * poly6Kernel(r, KERNEL_DISTANCE);
+      p.density += p_.mass * poly6Kernel(r);
     });
   });
 }
@@ -92,14 +138,14 @@ function computeAcceleration() {
     glm.vec2.copy(p.acc, GRAVITY);
     glm.vec2.zero(acc_pressure);
     glm.vec2.zero(acc_viscosity);
-    getNearNeighbors(p.pos, KERNEL_DISTANCE).forEach((p_) => {
+    hashNearNeighbors(p.pos).forEach((p_) => {
       glm.vec2.sub(r, p.pos, p_.pos);
-      acc_p_ = poly6Grad(r, KERNEL_DISTANCE);
+      acc_p_ = poly6Grad(r);
       glm.vec2.scale(acc_p_, acc_p_, ((p_.mass / p_.density) * (p.pressure + p_.pressure)) / 2);
       glm.vec2.add(acc_pressure, acc_pressure, acc_p_);
 
       glm.vec2.sub(acc_v_, p.vel, p_.vel);
-      glm.vec2.scale(acc_v_, acc_v_, (p_.mass / p_.density) * poly6Lap(r, KERNEL_DISTANCE));
+      glm.vec2.scale(acc_v_, acc_v_, (p_.mass / p_.density) * poly6Lap(r));
       glm.vec2.add(acc_viscosity, acc_viscosity, acc_v_);
     });
     glm.vec2.scaleAndAdd(p.acc, p.acc, acc_pressure, -1 / p.density);
@@ -133,6 +179,7 @@ function handleBoundaries() {
 }
 
 function simulate() {
+  updateHashTable();
   computeDensity();
   computePressure();
   computeAcceleration();
@@ -154,7 +201,7 @@ window.onload = () => {
   sizeCanvas();
 
   // Initialize
-  initParticles(KERNEL_DISTANCE);
+  initParticles(5);
 
   // Schedule the main animation loop
   animate();
